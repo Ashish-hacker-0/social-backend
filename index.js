fc1e2cd2b6f3344d64da1d0cfb84c6657baf2106
Ashish-hacker-0 <ashish1222001@gmail.com>
2021-08-22 16:30:20 +0530

    start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-let port = process.env.PORT ||3000 ;
+let port = process.env.PORT || 8001 ;
 
 app.listen(port,function(req,res){
     console.log('Server started at ',port);
